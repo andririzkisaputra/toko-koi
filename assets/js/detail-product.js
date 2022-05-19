f5020e2018e4ebbda08904c844f1ada56c5b9125
andririzki <andri.rizki007@gmail.com>
2022-05-19 12:06:10 +0700

    js product dan detail-product

--- assets/js/detail-product.js
+++ assets/js/detail-product.js
@@ -57,6 +57,8 @@ $(document).ready(function() {
                     +'<h3>Detail</h3>'
                     +'<ul>'
                         +'<li><strong>Nama</strong>: '+val.name+'</li>'
+                        +'<li><strong>Harga</strong>: '+formatRupiah(val.price, 'Rp ')+'</li>'
+                        +'<li><strong>Stok</strong>: '+val.stock+'</li>'
                         +'<li><strong>Tanggal Produk</strong>: '+moment(val.created_at).format('DD MMMM YYYY')+'</li>'
                     +'</ul>'
                 +'</div>'
@@ -70,4 +72,21 @@ $(document).ready(function() {
         data = data.toString();
         $('.details').html(data);
     }
+
+    function formatRupiah(angka, prefix){
+        var number_string = angka.replace(/[^,\d]/g, '').toString(),
+        split   		= number_string.split(','),
+        sisa     		= split[0].length % 3,
+        rupiah     		= split[0].substr(0, sisa),
+        ribuan     		= split[0].substr(sisa).match(/\d{3}/gi);
+
+        if(ribuan){
+            separator = sisa ? '.' : '';
+            rupiah += separator + ribuan.join('.');
+        }
+
+        rupiah = split[1] != undefined ? rupiah + ',' + split[1] : rupiah;
+        return prefix == undefined ? rupiah : (rupiah ? 'Rp. ' + rupiah : '');
+    }
+
 });
--- assets/js/home.js
+++ assets/js/home.js
@@ -11,22 +11,28 @@ $(document).ready(function() {
                 limit: limit,
             },
             success: function(res){
-                let info = '';
-                res.data.product.map(function(val, index){
+                let info;
+                res.data.map(function(val, index){
                     if (user) {
-                        info = '<a href="config/frontend/create-cart.php?product='+val.code_product+'" class="preview-link"><i class="bx bx-cart"></i></a>'
-                            +'<a href="product-details.php?product='+val.code_product+'" class="details-link"><i class="bx bx-spreadsheet"></i></a>';
+                            if (val.qty) {
+                                info = '<a href="javascript:void(0)" onclick="minCart(this)" data-product="'+val.code_product+'" class="preview-link"><i class="bx bx-caret-left"></i></a>'
+                                    +'<a href="javascript:void(0)" class="preview-link"><span>'+val.qty+'</span></a>'
+                                    +'<a href="javascript:void(0)" onclick="maxCart(this)" data-product="'+val.code_product+'" class="preview-link"><i class="bx bx-caret-right"></i></a>';
+                            } else {
+                                info = '<a href="config/frontend/create-cart.php?product='+val.code_product+'" class="preview-link"><i class="bx bx-cart"></i></a>';
+                            }
+                            info += '<a href="product-details.php?product='+val.code_product+'" class="details-link"><i class="bx bx-spreadsheet"></i></a>';
                     } else {
                         info = '<a href="product-details.php?product='+val.code_product+'" class="preview-link"><i class="bx bx-spreadsheet"></i></a>';
                     }
                     data.push(
-                    '<div class="product-item product-app" style="background-image: url(assets/img/product/'+val.name+'); padding: 0px;">'
-                        +'<div class="product-info">'
-                            +'<h4>'+val.name_product+'</h4>'
-                            +'<p>'+val.description.substring(0, 40)+'...</p>'
-                            +info
+                        '<div class="product-item product-app" style="background-image: url(assets/img/product/'+val.name+'); padding: 0px;">'
+                            +'<div class="product-info">'
+                                +'<h4>'+val.name_product+'</h4>'
+                                +'<p>'+val.description.substring(0, 40)+'...</p>'
+                                +info
+                            +'</div>'
                         +'</div>'
-                    +'</div>'
                     )
                 })
                 data = data.join('');
@@ -36,10 +42,37 @@ $(document).ready(function() {
             }
         })
     }
-
     $('#more button').click(function() {
         limit = 0;
         data = [];
         get();
     });
 });
+
+function minCart(data) {
+    $.ajax({
+        url: 'config/frontend/min-cart.php',
+        method: 'POST', 
+        dataType: 'json',
+        data: {
+            product: $(data).data("product"),
+        },
+        success: function(res){
+            return location.reload();
+        }
+    })
+}
+
+function maxCart(data) {
+    $.ajax({
+        url: 'config/frontend/max-cart.php',
+        method: 'POST', 
+        dataType: 'json',
+        data: {
+            product: $(data).data("product"),
+        },
+        success: function(res){
+            return location.reload();
+        }
+    })
+}
